0cc7de9ef5014f39197be75d828a913cec189861
AktamNosirov <94_aktam@mail.ru>
2022-03-12 21:46:07 +0500

    homework3

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,7 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number=23
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
 #Print the answer variable
+a=number%10
+b=a%10
+print(b*10+a)
